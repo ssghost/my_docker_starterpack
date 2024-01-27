5e6230bb230dfe5808ad4405176eb76b056258e0
Stardust Song <6882971+ssghost@users.noreply.github.com>
2024-01-27 10:11:08 +0800

    Update surdb.sh

--- surdb.sh
+++ surdb.sh
@@ -1,1 +1,1 @@
-docker run -d --rm --name surrealdb -p 8000:8000 surrealdb/surrealdb:latest start --log trace --user root --pass root memory
+docker run -d --name surrealdb -p 8000:8000 surrealdb/surrealdb:latest start --log trace --user root --pass root memory
