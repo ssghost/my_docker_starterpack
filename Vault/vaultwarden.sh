3f237642f4d78864135f298ee9ba8466e2d50c65
Stardust Song <6882971+ssghost@users.noreply.github.com>
2024-06-17 16:46:47 +0800

    Update vaultwarden.sh

--- Vault/vaultwarden.sh
+++ Vault/vaultwarden.sh
@@ -1,1 +1,1 @@
-docker run -d --name vaultwarden -e ADMIN_TOKEN=RLQk7bexZYxaMIqAcj/7rMBIvnAFvZYiLUuBXQ1IdyW0L4fH6j81oHg6myDSe2yN -e ROCKET_TLS='{certs="/ssl/MyCertificate.pem",key="/ssl/MyKey.pem"}' -v /Users/songshuang/Documents/vw-data/certs/:/ssl/ -v /Users/songshuang/Documents/vw-data:/data/ -p 3096:80 vaultwarden/server
+docker run -d --name vaultwarden -e ADMIN_TOKEN="MY_TOKEN" -e ROCKET_TLS='{certs="/ssl/MyCertificate.pem",key="/ssl/MyKey.pem"}' -v /~/Documents/vw-data/certs/:/ssl/ -v /~/Documents/vw-data:/data/ -p 3096:80 vaultwarden/server
