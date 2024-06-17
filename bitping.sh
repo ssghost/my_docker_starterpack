9583696f06e3b0605406609b86adc16f15d4935e
Stardust Song <6882971+ssghost@users.noreply.github.com>
2024-06-17 16:49:45 +0800

    Update bitping.sh

--- bitping.sh
+++ bitping.sh
@@ -1,1 +1,1 @@
-docker run -d --hostname=ee2080f75dff --user=0 --mac-address=02:42:ac:11:00:04 --env=PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin --env=SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt --workdir=/ --restart=no --runtime=runc -t -d bitping/bitping-node:latest
+docker run -d -e BITPING_EMAIL='My_EMAIL' -e BITPING_PASSWD='MY_PASSWORD' -v "$PWD/.data/.bitpingd:/root/.bitpingd" --name bitping mrcolorrain/bitping
